6776a02d30bd49d3e6684cf597bfaf4eefc28cd8
thiepn <junsosmart@gmail.com>
2026-08-27 23:36:24 +0200

    build: load Phase 14 accessibility layer

--- src/main.tsx
+++ src/main.tsx
@@ -38,6 +38,7 @@ import "./styles/v24-explore-map.css";
 import "./styles/v25-countries-languages.css";
 import "./styles/v26-prayer-saved.css";
 import "./styles/v27-account-ux.css";
+import "./styles/v28-accessibility.css";
 
 initializeOfflineRuntime();
 initializePrivateSyncRuntime();
