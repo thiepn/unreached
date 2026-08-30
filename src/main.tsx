61d8df5ae4fadbb9f4bf7f469fce785a21186867
thiepn <junsosmart@gmail.com>
2026-08-31 01:10:37 +0200

    Load newcomer Explore styles

--- src/main.tsx
+++ src/main.tsx
@@ -56,6 +56,7 @@ import "./styles/foundation/detail-records.css";
 import "./styles/prayer/guides-and-lists.css";
 import "./styles/account/ux.css";
 import "./styles/comprehension.css";
+import "./styles/explore/newcomer-entry.css";
 import "./styles/foundation/accessibility.css";
 
 // Start the single prepared IndexedDB read as soon as the entry module runs.
