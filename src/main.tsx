7a3301d55f2ae5121c747394f6c8d892221cd916
thiepn <junsosmart@gmail.com>
2026-09-16 22:55:23 +0200

    Load V3 visual foundation styles

--- src/main.tsx
+++ src/main.tsx
@@ -57,6 +57,12 @@ import "./styles/prayer/guides-and-lists.css";
 import "./styles/account/ux.css";
 import "./styles/comprehension.css";
 import "./styles/explore/newcomer-entry.css";
+import "./styles/v3/tokens.css";
+import "./styles/v3/typography.css";
+import "./styles/v3/layout.css";
+import "./styles/v3/components.css";
+import "./styles/v3/atlas.css";
+import "./styles/v3/responsive.css";
 import "./styles/foundation/accessibility.css";
 
 // Start the single prepared IndexedDB read as soon as the entry module runs.
